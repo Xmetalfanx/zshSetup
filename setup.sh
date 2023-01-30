609cc39e29bf8e53839e02a6f019f7ba501ce9e6
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-01-30 03:50:43 -0500

    Working Plugin setup and theme setup

--- setup.sh
+++ setup.sh
@@ -1,53 +1,101 @@
-zshConfigDir="~/.config/zsh"
-zshConfigFile="~/.zshrc"
+#!/bin/bash
+
+zshConfigDir="/home/${USER}/.config/zsh"
+zshConfigFile="/home/${USER}/.zshrc"
+
+
+function backupZSHRC() {
+    echo -e "Backing up existing zshrc"
+    cp "${zshConfigFile}" "${zshConfigFile}.backup"
+
+}
+backupZSHRC
+
+function clearZSHRC() {
+    /dev/null > ${zshConfigFile}
+}
+clearZSHRC
+
+
+
+#########################################################################################
+
+# Plugin related 
+
 
 # TODO: backup existing zshrc file if it exists 
 
+# zsh-users is fine 
+# ohmyzsh has plugins in a sub-folder ... that may be an issue 
+
+function userPrompt() {
+    read -p "Press Any Key to continue"
+}
 
 
-function downloadPlugin() {
+function downloadZshusersPlugin() {
+    pluginName=${1}
+
+    localZshUsersDir="${zshConfigDir}/${pluginName}"
     
+    for currentPlugin in "${@}"
+        do 
+            # https://github.com/zsh-users/zsh-autosuggestions
+            # zsh-users
+            gitPluginURL="https://github.com/${repoName}/${currentPlugin}"
+            
+            echo -e "Downloading ${currentPlugin} to ${zshConfigDir}"
+            [ ! -d "${localZshUsersDir}" ] && git clone -q "${gitPluginURL}" "${zshConfigDir}/${currentPlugin}"
+        done 
+
+    setupPlugins "${@}"
+}
+
+function downloadOhMyZSHPlugin() {
+    # here "repoName" is set already 
+    ohmyzshRepo="https://github.com/ohmyzsh/ohmyzsh.git"
+    localOhMyZshDir="assets/ohmyzsh"
+
+    [ ! -d ${localOhMyZshDir} ] && git clone -q "${ohmyzshRepo}" "${localOhMyZshDir}"
+    
+    ohmyzshPluginsDir="${localOhMyZshDir}/plugins/"
 
     for currentPlugin in "${@}"
     do 
+        currentPluginLocalDir="${ohmyzshPluginsDir}${currentPlugin}"
 
-        echo -e "repoName:\t${repoName}"
-        # from ohmhzsh repo 
-        [ "${repoName}" == "ohmyzsh" ] && rawPlugin="https://raw.githubusercontent.com/${repoName}/${repoName}/master/plugins/${currentPlugin}/${currentPlugin}.plugin.zsh"
-    
+        [ -d "${currentPluginLocalDir}" ] && echo -e "Copying ${currentPluginLocalDir} to ${zshConfigDir}"
+        cp -r "${currentPluginLocalDir}" "${zshConfigDir}"
 
-        # from zsh-users repo 
-        [ "${repoName}" == "zsh-users" ] && rawPlugin="https://raw.githubusercontent.com/${repoName}/${currentPlugin}/master/${currentPlugin}.zsh"
-
-
-        echo -e "${rawPlugin}"
-        
-        wget =s -P "assets/" "${rawPlugin}" 
-        sleep 1 
-
+        # for debugging 
+        #ls -al ${zshConfigDir}
 
     done 
 
+    setupPlugins "${@}"
 }
 
+
+# setup .zshrc file 
 function setupPlugins() {
     
-    #echo -e "Copying zsh plugins to config location"
-    #cp assets/config/zsh ~/.config/zsh/
-
     echo -e "Setting up plugins in .zshrc file"
-    echo -e "#Include Plugins\t"
-
+    
     for currentPlugin in "${@}"
     do 
-        #echo -e "source ${zshConfigDir}/${currentPlugin}/${currentPlugin}.plugin.zsh" >> ${zshConfigFile}
-        echo -e "source ${zshConfigDir}/${currentPlugin}/${currentPlugin}.plugin.zsh" 
-
+        currentPluginScriptFile="${zshConfigDir}/${currentPlugin}/${currentPlugin}.plugin.zsh"
+        echo -e "${currentPluginScriptFile}\v"
+        
+        echo -e "setting up ${currentPlugin} in zsh config file "
+        echo -e "source ${currentPluginScriptFile}" >> ${zshConfigFile}
+        sleep 2
     done 
-
 
 }
 
+
+##################################################################################3
+# functions that use the functions above 
 
 function ohmyzshPlugins() { 
     # - DirHistory: https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/dirhistory
@@ -55,7 +103,7 @@ function ohmyzshPlugins() {
 
     repoName="ohmyzsh"
 
-    downloadPlugin "dirhistory" "sudo"
+    downloadOhMyZSHPlugin "dirhistory" "sudo"
 }
 
 function zshUserPlugins() {
@@ -68,16 +116,22 @@ function zshUserPlugins() {
 
     repoName="zsh-users"
 
-    downloadPlugin "zsh-autosuggestions" "zsh-history-substring-search" "zsh-syntax-highlighting"
+    downloadZshusersPlugin "zsh-autosuggestions" "zsh-history-substring-search" "zsh-syntax-highlighting"
 
 }
 
 ohmyzshPlugins
 zshUserPlugins
 
+# Plugin related 
+#####################################################################
+
+##################################################################################################
+
+
 # Set up zsh history 
 function createHistoryLocation() {
-    cacheDir="~/.cache/.zsh/"
+    cacheDir="/home/${USER}/.cache/.zsh/"
     zshHistoryFile="${cacheDir}/history"
 
     echo -e "Creating History file to use"
@@ -87,21 +141,20 @@ function createHistoryLocation() {
     touch "${zshHistoryFile}"
 
     # History in cache directory:
-    echo "# History file\nHISTFILE=~/.cache/zsh/history" > ${zshConfigFile}
+    echo -e "# History file\nHISTFILE=~/.cache/zsh/history" > "${zshConfigFile}"
     
 }
-
-
 
 
 # Setip Prompt/Theme
 function setupPrompt() {
     
     # NOTE TO SELF: remember to use \" for the lines below when echoing out
-    echo "Setting up Prompt UI (Inspired by Fish/Oh-My-Fish's BobTheFish theme"
-    echo "#Prompt UI\n#PROMPT=\"%K{#333333}%M@%B %F{yellow}%1~ #%f %b%k\"\nPROMPT=\"%K{#333333}%M@%B %F{green}%1~ #%f %b%k\"\n"
+    echo -e "Setting up Prompt UI (Inspired by Fish/Oh-My-Fish's BobTheFish theme"
+    echo -e "\n#Prompt UI\n#PROMPT=\"%K{#333333}%M@%B %F{yellow}%1~ #%f %b%k\"\nPROMPT=\"%K{#333333}%M@%B %F{green}%1~ #%f %b%k\"\n" >> ${zshConfigFile}
     
 }
+setupPrompt
 
 #################################################
 # Alias related 
@@ -136,5 +189,8 @@ function setupGitAliases() {
     alias gac="git add . -- && clear"
 
     # i dont want to mess up git repos but could i combine aliases?
-    # example ' gacm="gac && gcm" ' ... so then i'd type ' gacm "foobar message"    '
+    # example - gacm="gac && gcm" ' ... so then i'd type ' gacm "foobar message"    '
 }
+
+
+source ~/.zshrc
